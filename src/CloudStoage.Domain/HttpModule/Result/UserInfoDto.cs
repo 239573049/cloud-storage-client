6f071cf060cfa4827fbb012b772c4f1736a557fa
239573049@qq.com <239573049@qq.com>
2022-07-28 22:40:59 +0800

    增加获取个人资料接口 完善个人资料展示

---
+++ src/CloudStoage.Domain/HttpModule/Result/UserInfoDto.cs
@@ -0,0 +1,80 @@
+﻿using CloudStorage.Domain.Shared;
+
+namespace CloudStoage.Domain.HttpModule.Result;
+
+public class UserInfoDto
+{
+    public Guid Id { get; set; }
+
+    /// <summary>
+    /// 账号
+    /// </summary>
+    public string? Account { get; set; }
+
+    /// <summary>
+    /// 密码
+    /// </summary>
+    public string? Password { get; set; }
+
+    /// <summary>
+    /// 昵称
+    /// </summary>
+    public string? Name { get; set; }
+
+    /// <summary>
+    /// 简介
+    /// </summary>
+    public string? BriefIntroduction { get; set; }
+
+    /// <summary>
+    /// 微信openid
+    /// </summary>
+    public string? WeChatOpenId { get; set; }
+
+    /// <summary>
+    /// 头像
+    /// </summary>
+    public string? HeadPortraits { get; set; }
+
+    /// <summary>
+    /// 性别
+    /// </summary>
+    public SexType Sex { get; set; }
+
+    /// <summary>
+    /// 状态
+    /// </summary>
+    public UserStatus Status { get; set; }
+
+    /// <summary>
+    /// 服务器文件
+    /// </summary>
+    public string? CloudStorageRoot { get; set; }
+
+    public DateTime CreationTime { get; }
+
+    /// <summary>
+    /// 用户总大小
+    /// </summary>
+    public long TotalSize { get; set; }
+
+    /// <summary>
+    /// 已经使用大小
+    /// </summary>
+    public long UsedSize { get; set; } = 0;
+
+    /// <summary>
+    /// 使用占比
+    /// </summary>
+    public int Percentage
+    {
+        get
+        {
+            if(UsedSize!=0 && TotalSize != 0)
+            {
+                return (int)((UsedSize / TotalSize) * 100);
+            }
+            return 0;
+        }
+    }
+}
--- src/CloudStorage.Layou/Pages/PersonalCenter.razor.cs
+++ src/CloudStorage.Layou/Pages/PersonalCenter.razor.cs
@@ -1,14 +1,32 @@
-﻿using Microsoft.AspNetCore.Components.Web;
+﻿using CloudStoage.Domain.HttpModule.Result;
+using CloudStorage.Applications.Helpers;
 
 namespace CloudStorage.Layou.Pages;
 
 partial class PersonalCenter
 {
+    private UserInfoDto? UserInfo { get; set; } =new UserInfoDto();
     [Inject]
     public NavigationManager? Navigation { get; set; }
+
+    [Inject]
+    public CommonHelper CommonHelper { get; set; }
+
+    [Inject]
+    public UserInfoApi UserInfoApi { get; set; }
 
     private void OnLogoutClick(MouseEventArgs args)
     {
         Navigation?.NavigateTo("/login");
+    }
+
+    protected override async Task OnInitializedAsync()
+    {
+        await GetUserInfoAsync();
+    }
+
+    private async Task GetUserInfoAsync()
+    {
+         UserInfo = await UserInfoApi.GetAsync();
     }
 }
