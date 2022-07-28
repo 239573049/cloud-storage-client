3b6093f33b30bd388292576414c0ac95236a0773
239573049@qq.com <239573049@qq.com>
2022-07-28 13:27:47 +0800

    api更换域名访问防止静态资源无法访问

--- src/CloudStorage.Domain.Shared/Constant.cs
+++ src/CloudStorage.Domain.Shared/Constant.cs
@@ -4,7 +4,7 @@ public class Constant
 {
     public const string Token = "token";
 
-    public const string Api = "https://124.222.27.83";
+    public const string Api = "https://tokenhu.top";
 
     public const string DateTimeStr = "yyyy-MM-dd HH-mm-ss";
 
